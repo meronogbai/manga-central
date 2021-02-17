a8c6ed0cfd17415a61d8f3153522072322167b43
Meron Ogbai <okbaymeron@gmail.com>
2021-02-17 10:01:15 +0300

    Add navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Error from './Error';
 import Home from './Home';
+import Navbar from './Navbar';
 
 const App = () => (
   <main>
     <BrowserRouter>
+      <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route component={Error} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+  <nav>
+    <Link to="/">Home</Link>
+  </nav>
+);
+
+export default Navbar;
