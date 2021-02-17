553b9f215676497156402dffa30ef1b156e1821a
Meron Ogbai <okbaymeron@gmail.com>
2021-02-17 20:00:26 +0300

    Add pagination buttons

--- src/components/MangaList.js
+++ src/components/MangaList.js
@@ -1,16 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import fetchManga from '../actions/fetchManga';
 
 const MangaList = () => {
+  const [page, setPage] = useState(1);
   const manga = useSelector(state => state.manga);
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(fetchManga(1));
-  }, []);
+    dispatch(fetchManga(page));
+  }, [page]);
+  const handleNextClick = () => {
+    setPage(page => page + 1);
+  };
+  const handlePreviousClick = () => {
+    setPage(page => (page === 1 ? page : page - 1));
+  };
   return (
     <div>
       <h1>Manga List</h1>
+      <button type="button" onClick={handlePreviousClick}>Previous</button>
+      <button type="button" onClick={handleNextClick}>Next</button>
       {manga.loading && 'Loading...'}
       {JSON.stringify(manga.items)}
     </div>
