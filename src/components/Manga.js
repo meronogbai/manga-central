fe56a217d212626ce14e75d3e53259c8c5bb55af
Meron Ogbai <okbaymeron@gmail.com>
2021-02-17 11:14:11 +0300

    Make api call in manga page

--- src/components/Manga.js
+++ src/components/Manga.js
@@ -1,13 +1,29 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Manga = () => {
   const { id } = useParams();
+  const [manga, setManga] = useState('Loading..');
+  const useFetch = id => {
+    useEffect(() => {
+      fetch(`https://api.jikan.moe/v3/manga/${id}`)
+        .then(res => res.json())
+        .then(data => setManga(JSON.stringify(data)));
+    }, []);
+  };
+
+  useFetch(id);
   return (
-    <h1>
-      Manga
-      {' '}
-      {id}
-    </h1>
+    <article>
+      <h1>
+        Manga
+        {' '}
+        {id}
+      </h1>
+      <div>
+        {manga}
+      </div>
+    </article>
   );
 };
 
