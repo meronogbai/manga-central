ee4d1cd69ebdec7c01d58c65692e1887bcf2f500
Meron Ogbai <okbaymeron@gmail.com>
2021-02-17 18:38:35 +0300

    Create store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
 
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk),
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
