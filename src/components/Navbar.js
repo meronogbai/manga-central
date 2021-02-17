bb2295db2d6a2fc42a3b3fd9b6ca838e7df401bf
Meron Ogbai <okbaymeron@gmail.com>
2021-02-17 21:34:09 +0300

    Add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,19 @@
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 import styles from '../styles/Navbar.module.css';
 
 const Navbar = () => (
   <nav className={styles.navbar}>
-    <Link to="/">Home</Link>
-    <Link to="/manga">Manga List</Link>
+    <Link to="/"><FontAwesomeIcon icon={faBook} size="2x" className={styles.logo} /></Link>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/manga">Manga List</Link>
+      </li>
+    </ul>
   </nav>
 );
 
