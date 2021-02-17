aef216ce970b90ebc941599a0f139e73af90fe7c
Meron Ogbai <okbaymeron@gmail.com>
2021-02-17 18:57:55 +0300

    Setup redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
 
-const store = createStore(
-  rootReducer,
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(
   applyMiddleware(thunk),
-);
+));
 
 ReactDOM.render(
   <React.StrictMode>
