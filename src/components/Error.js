be42c665a3fdd7a420f8f9eae67e1af9bfb2312e
Meron Ogbai <okbaymeron@gmail.com>
2021-02-18 22:14:46 +0300

    Style error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,3 +1,13 @@
-const Error = () => (<h1>error</h1>);
+import styles from '../styles/Error.module.css';
+
+const Error = () => (
+  <main className={styles.error}>
+    <div>
+      <h1>Oops!</h1>
+      <p>Seems like you&apos;ve encoutered an error.</p>
+      <img src="https://i.kym-cdn.com/photos/images/original/001/401/347/312.jpg" alt="Error Meme" />
+    </div>
+  </main>
+);
 
 export default Error;
