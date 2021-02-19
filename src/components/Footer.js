04455017efe5b8fd9682d89537d025fa0280734f
Meron Ogbai <okbaymeron@gmail.com>
2021-02-19 11:28:59 +0300

    Keep footer at the bottom

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,13 @@
-import { useLocation } from 'react-router-dom';
 import styles from '../styles/Footer.module.css';
 
-const Footer = () => {
-  const location = useLocation();
-  const bottom = location.pathname !== '/manga' ? styles.fixedBottom : '';
-  return (
-    <footer className={`${styles.footer} ${bottom}`}>
-      Made with
-      {' '}
-      <span role="img" aria-label="heart">❤️</span>
-      {' '}
-      by Meron Ogbai
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className={styles.footer}>
+    Made with
+    {' '}
+    <span role="img" aria-label="heart">❤️</span>
+    {' '}
+    by Meron Ogbai
+  </footer>
+);
 
 export default Footer;
